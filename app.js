ed8ef146abcea759927e8e176de5af76e5fbb2de
carolgreene <carolgreene3@gmail.com>
2020-05-02 12:40:18 -0700

    add js to make calculations

---
+++ app.js
@@ -0,0 +1,33 @@
+//listen for submit
+document.querySelector('#loan-form').addEventListener("submit", calculateResults);
+
+//calculate results
+function calculateResults(e) {
+  e.preventDefault()
+  //UI variables
+  const amount = document.getElementById('amount')
+  const interest = document.getElementById('interest')
+  const years = document.getElementById('years')
+  const monthlyPayment = document.getElementById('monthly-payment')
+  const totalPayment = document.getElementById('total-payment')
+  const totalInterest = document.getElementById('total-interest')
+
+  //calculations
+  const principal = parseFloat(amount.value);  //parseFloat will give it decimal
+  const calculatedInterest = parseFloat(interest.value)/100/12
+  const calculatedPayments = parseFloat(years.value) * 12
+
+  //compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+  const monthly = (principal*x*calculatedInterest)/(x-1)
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2)
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2)
+    totalInterest.value = ((monthly* calculatedPayments)-principal).toFixed(2)
+  } else {
+    showError("Please check your numbers")
+  }
+}
+
+
